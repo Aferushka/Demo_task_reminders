6178bf3a0f958664e1dd52204f0f2355ea0e7540
Aferushka <myevska@gmail.com>
2020-12-05 17:48:18 +0300

    debugging celery-beat

--- reminders_back/reminders_back/celery.py
+++ reminders_back/reminders_back/celery.py
@@ -1,18 +1,21 @@
 from __future__ import absolute_import
 import os
 from celery import Celery
-from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reminders_back.settings')
 
 app = Celery('reminders_back')
 app.config_from_object('reminders_back.celeryconfig')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
 
+app.conf.beat_schedule = {
+    'test_task_10_sec': {
+        'task': 'try_task',
+        'schedule': 5.0,
+        'args': ()
+    }
+}
 
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(10, try_task(), name='test task 10 sec')
 
 @app.task
 def try_task():
