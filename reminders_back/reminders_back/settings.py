c3fbc3309e49617cd247696ddd49a7164f6efa3a
Aferushka <myevska@gmail.com>
2020-12-05 21:49:35 +0300

    changed sqlite to postgres

--- reminders_back/reminders_back/settings.py
+++ reminders_back/reminders_back/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'reminders_back.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite'
     }
 }
 
