20d68d8ae8366a1f583ba2ba351871be541f5d9c
Aferushka <myevska@gmail.com>
2020-12-05 17:37:00 +0300

    debugging celery-beat

--- reminders_back/reminders/tasks.py
+++
@@ -1,6 +0,0 @@
-from celery import shared_task
-
-
-@shared_task()
-def try_task():
-    print('Its working!')
--- reminders_back/reminders_back/celery.py
+++ reminders_back/reminders_back/celery.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 import os
 from celery import Celery
 from django.conf import settings
-from reminders.tasks import try_task
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reminders_back.settings')
 
@@ -14,3 +13,7 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(10, try_task(), name='test task 10 sec')
+
+@app.task
+def try_task():
+    print('Its working!')
