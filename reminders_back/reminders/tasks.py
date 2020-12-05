58b1efcadcc540a8d706ed3e388e419d4edb49b5
Aferushka <myevska@gmail.com>
2020-12-05 23:44:49 +0300

    qfix

--- reminders_back/reminders/tasks.py
+++ reminders_back/reminders/tasks.py
@@ -9,10 +9,15 @@ def reminder_scan():
     call_time = datetime.today()
     for reminder in Reminder.objects.all():
         if reminder.date == call_time.date() and reminder.time.hour == call_time.hour and reminder.time.minute == call_time.minute:
-            send_reminder.delay(reminder)
+            send_reminder.delay(reminder.id)
 
 
 @shared_task
-def send_reminder(reminder):
+def send_reminder(reminder_id):
+    try:
+        reminder = Reminder.objects.get(id=reminder_id)
+    except:
+        return
+
     for chat in ChatId.objects.all():
         bot.sendMessage(chat_id=chat.chat_id, text=reminder.text)
